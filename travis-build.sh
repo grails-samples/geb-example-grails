a3f98601ab7ef8b25763c32a02443a59ef906cb2
sdelamo <sergio.delamo@softamo.com>
2017-12-19 13:18:30 +0100

    Attemp to install chrome driver in travis

--- travis-build.sh
+++ travis-build.sh
@@ -4,6 +4,6 @@ echo "Running tests..."
 
 EXIT_STATUS=0
 
-./gradlew check -Dgeb.env=chromeHeadless || EXIT_STATUS=$?
+./gradlew check -Dgeb.env=chromeHeadless -Dwebdriver.chrome.driver=/usr/local/share/chromedriver || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
