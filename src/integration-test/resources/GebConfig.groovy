c0ebee3fadcc7283e141fab3e7ac8fd490d86a04
Sergio del Amo <sergio.delamo@softamo.com>
2018-06-23 12:50:13 +0200

    Missing FirefoxOptions

--- src/integration-test/resources/GebConfig.groovy
+++ src/integration-test/resources/GebConfig.groovy
@@ -1,6 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 
 environments {
 
