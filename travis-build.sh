5a9357690ad69261f0407c9be85657f85f1eceab
sdelamo <sergio.delamo@softamo.com>
2018-10-12 16:43:10 +0200

    try to run in firefox

--- travis-build.sh
+++ travis-build.sh
@@ -6,6 +6,6 @@ EXIT_STATUS=0
 
 ./gradlew -Dgeb.env=chromeHeadless check || EXIT_STATUS=$?
 
-#./gradlew -Dgeb.env=firefoxHeadless iT || EXIT_STATUS=$?
+./gradlew -Dgeb.env=firefoxHeadless iT || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
