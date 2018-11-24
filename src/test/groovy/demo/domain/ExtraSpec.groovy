e5e098b6253eb040d8c7180ec381672ec03540e0
sdelamo <sergio.delamo@softamo.com>
2018-11-24 10:24:45 +0100

    Revert "failing Tests"
    This reverts commit fe157799b785d032c67916b912661e6e4f50de9a.

--- src/test/groovy/demo/domain/ExtraSpec.groovy
+++ src/test/groovy/demo/domain/ExtraSpec.groovy
@@ -1,16 +1,42 @@
 package demo.domain
 
+import demo.Extra
+import grails.testing.gorm.DomainUnitTest
 import spock.lang.Specification
 
-class ExtraSpec extends Specification {
+class ExtraSpec extends Specification implements DomainUnitTest<Extra> {
 
-    void 'test name cannot be null for Extra domain class'() {
+    void 'test name cannot be null'() {
+        given:
+        domain.name = null
+
         expect:
-        false
+        !domain.validate(['name'])
+        domain.errors['name'].code == 'nullable'
     }
 
-    void 'test name can have a maximum of 255 characters for Extra domain class'() {
+    void 'test name cannot be blank'() {
+        given:
+        domain.name = ''
+
         expect:
-        false
+        !domain.validate(['name'])
+    }
+
+    void 'test name can have a maximum of 255 characters'() {
+        given:
+        domain.name = name
+
+        expect:
+        isValid == domain.validate(['name'])
+
+        if (!isValid) {
+            assert domain.errors['name'].code == 'maxSize.exceeded'
+        }
+
+        where:
+        name       | isValid
+        'a' * 256  | false
+        'a' * 255  | true
     }
 }
